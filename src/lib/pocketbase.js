d76ddf9c4533fdc29e26e03e2ba3afdae20cccb5
Victor Elgersma <victor.elgersma41@thg.com>
2024-07-11 21:40:31 +0100

    initial connection to pocketbase instance

---
+++ src/lib/pocketbase.js
@@ -0,0 +1,7 @@
+
+import PocketBase from "pocketbase";
+const url = "https://be-proud-thunder-3266.fly.dev/";
+
+const client = new PocketBase(url);
+
+export default client;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import client from '../lib/pocketbase'
+
 
 export default function Home() {
+  useEffect(() => {
+    client.collection("tasks").getFullList().then(res => console.log(res))  
+  }, [])
   return (
     <div>Home</div>
   )
